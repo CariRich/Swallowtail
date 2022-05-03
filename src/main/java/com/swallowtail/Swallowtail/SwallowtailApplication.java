e6b5ee48884ed473e155a5881e2e3916682d85b4
Carissa Richards <carissarichards@Carissas-MacBook-Pro.local>
2022-05-03 18:45:35 -0400

    Initial commit

---
+++ src/main/java/com/swallowtail/Swallowtail/SwallowtailApplication.java
@@ -0,0 +1,13 @@
+package com.swallowtail.Swallowtail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwallowtailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwallowtailApplication.class, args);
+	}
+
+}
